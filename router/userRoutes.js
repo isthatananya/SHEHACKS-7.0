cf066788ab2be693cf07f0c2d1774b245c81e113
pajju <prajwalshah.2003@gmail.com>
2024-04-09 19:58:17 +0530

    adding db to app

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 
-router.get('/', UserController.getAllUsers);
+// router.get('/', UserController.getAllUsers);
 
-router.get('/:userId', UserController.getUserById);
+// router.get('/:userId', UserController.getUserById);
 
-router.post('/', UserController.createUser);
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
 
-router.put('/:userId', UserController.updateUser);
 
-router.delete('/:userId', UserController.deleteUser);
 
 module.exports = router;
