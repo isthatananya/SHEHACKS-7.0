c4537fa2951d7415556ae5a42da08a62a382d221
pajju <prajwalshah.2003@gmail.com>
2024-04-09 19:32:15 +0530

    adding db to app

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const connect = require('./db/connect');
+
+
 app.get('/', (req, res) => {
   res.send('Server is on!');
 });
 
+async function start(){
+  await connect();
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}, you may visit http://localhost:${port}`);
+  });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}, you may visit http://localhost:${port}`);
-});
+}
+start();
